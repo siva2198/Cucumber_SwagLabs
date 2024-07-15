24c124c377dffe52e40209514678fae6dc4a1993
Sivaraman M <sivaraman.m@moxey.ai>
2024-07-15 20:52:48 +0530

    integration of allure and tested

--- src/test/java/com/swaglabs/Runner/TestRunner.java
+++ src/test/java/com/swaglabs/Runner/TestRunner.java
@@ -8,9 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/java/com/swaglabs/Features",
         glue = {"com.swaglabs.StepDefinition", "com.swaglabs.BaseConfig"},
-        plugin = {"pretty", "html:target/cucumber-html-report.html", "json:cucumber.json"}
+        plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
-
+//"pretty", "html:target/cucumber-html-report.html","json:cucumber.json",
 public class TestRunner {
-
 }
